428abcb76458a0e003854a847290a3f8410102f4
davidcurral <davidcurral.work@gmail.com>
2026-09-23 19:18:32 +0100

    2º Database
    Create a second db with cards with variable cooldowns. Changing values in the databases change the cards themselves.

--- procedural-narrative/Scripts/cards_static.gd
+++ procedural-narrative/Scripts/cards_static.gd
@@ -1,5 +1,5 @@
 extends Node
 
 #@export var initial_card_list: Array[Cards]
-@onready var database = load("res://Resources/card_database_V1_5_turns_cooldown.tres")
+@onready var database = load("res://Resources/card_database_V2.tres")
 @export var max_concurrent_arcs: int = 2
